92fc47625ea454c53b9f3e8117e93cae7b9a3d73
Edilbert Nunes <edilbert.nunes@gmail.com>
2022-12-13 15:45:58 -0300

    RxJs Part 1

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators'
+import { resolve } from 'url';
 
 @Injectable({
   providedIn: 'root'
@@ -9,24 +12,27 @@ export class PromotionService {
 
   constructor() { }
 
-  getPromotions(): Promise<Promotion[]> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(PROMOTIONS), 2000);
-    });
+  getPromotions(): Observable<Promotion[]> {
+    return of(PROMOTIONS).pipe(delay(2000));
+    // return new Promise(resolve=> {
+    //   // Simulate server latency with 2 second delay
+    //     setTimeout(() => resolve(PROMOTIONS), 2000);
+    // });
   }
 
-  getPromotion(id: string): Promise<Promotion> {
-    return new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 2000);
-    });
+  getPromotion(id: string): Observable<Promotion> {
+    return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));
+    // return new Promise(resolve=> {
+    //   // Simulate server latency with 2 second delay
+    //     setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 2000);
+    // });
   }
 
-  getFeaturePromotions(): Promise<Promotion> {
-    return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(PROMOTIONS.filter((promo) => promo.featured)[0]), 2000);
-    });
+  getFeaturePromotions(): Observable<Promotion> {
+    return of(PROMOTIONS.filter((promo) => promo.featured)[0]).pipe(delay(2000));  
+    // return new Promise(resolve=> {
+    //   // Simulate server latency with 2 second delay
+    //     setTimeout(() => resolve(PROMOTIONS.filter((promo) => promo.featured)[0]), 2000);
+    // });
   }
 }
